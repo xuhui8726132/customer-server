3dd36e49c2321f3f97033f037640d289e5819301
xuhui8726132 <xuhui163email@163.com>
2018-08-19 17:18:22 +0800

    Initial commit

---
+++ src/main/java/com/xuhui/CustomerServerApplication.java
@@ -0,0 +1,12 @@
+package com.xuhui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServerApplication.class, args);
+	}
+}
