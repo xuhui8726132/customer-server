38da53e424873da0c54a19058d29927831fbf3d9
xuhui8726132 <xuhui163email@163.com>
2018-08-21 22:17:40 +0800

    333

--- src/main/java/com/xuhui/CustomerServerApplication.java
+++ src/main/java/com/xuhui/CustomerServerApplication.java
@@ -2,8 +2,10 @@ package com.xuhui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerServerApplication {
 
 	public static void main(String[] args) {
